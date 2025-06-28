ca15702f56694740f9c83a8d55275730fee03a85
GaganSalian <gagansalian04102005@gmail.com>
2025-06-28 11:21:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-var tl = gsap.timeline({scrollTrigger:{
-    trigger:"#main",
-   //  markers:true,
-    start:"50% 50%",
-    end:"150% 50%",
-    scrub:2,
-    pin:true
-}});
-tl
-.to("#center",{
-   height: "100vh",
-},'a')
-.to("#top",{
-    top: "-50%",
- },'a')
- .to("#bottom",{
-    bottom: "-50%",
- },'a')
-.to("#top-h1",{
-    top: "30%"
- },'a')
- .to("#bottom-h1",{
-    bottom: "-30%"
- },'a')
-.to("#center-h1",{
-   top: "-30%"
-},'a')
-.to(".content",{
-   delay: -0.5,
-   marginTop: "0%"
-})
+gsap.matchMedia().add("(min-width: 481px)", () => {
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#main",
+      start: "50% 50%",
+      end: "150% 50%",
+      scrub: 2,
+      pin: true
+    }
+  });
 
+  tl
+    .to("#center", { height: "100vh" }, 'a')
+    .to("#top", { top: "-50%" }, 'a')
+    .to("#bottom", { bottom: "-50%" }, 'a')
+    .to("#top-h1", { top: "30%" }, 'a')
+    .to("#bottom-h1", { bottom: "-30%" }, 'a')
+    .to("#center-h1", { top: "-30%" }, 'a')
+    .to(".content", { delay: -0.5, marginTop: "0%" });
+});
+
+// universal animation for all screens
 gsap.from(".header-text", {
   duration: 1.5,
   y: 50,
   opacity: 0,
   ease: "power3.out",
 });
+
 
 
 
